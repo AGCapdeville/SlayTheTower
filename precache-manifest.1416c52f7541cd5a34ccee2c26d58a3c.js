5189b2982693267fc9c70b5f866657a929827336
Adam Capdeville <agcapdeville@gmail.com>
2020-05-14 19:09:04 -0700

    Updates

---
+++ precache-manifest.1416c52f7541cd5a34ccee2c26d58a3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ffbe2589fc587aa36fd5d0096aa6689",
+    "url": "/index.html"
+  },
+  {
+    "revision": "744b9c352abdf097ec8d",
+    "url": "/static/css/main.438786c8.chunk.css"
+  },
+  {
+    "revision": "d75348c5d7d60d13d1eb",
+    "url": "/static/js/2.e1ed2a78.chunk.js"
+  },
+  {
+    "revision": "6c46089e713fdf01ec7e89132bc85450",
+    "url": "/static/js/2.e1ed2a78.chunk.js.LICENSE"
+  },
+  {
+    "revision": "744b9c352abdf097ec8d",
+    "url": "/static/js/main.d4cb2c63.chunk.js"
+  },
+  {
+    "revision": "53d2d4aab2213da131d0",
+    "url": "/static/js/runtime-main.29a961ca.js"
+  }
+]);
